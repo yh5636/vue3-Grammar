669141eb64a254e657d841bc64947c2d31793ef3
yh5636 <yh5636@naver.com>
2022-06-30 15:42:36 +0900

    vue3 컴포지션

---
+++ src/App_BasicGrammer.vue
@@ -0,0 +1,52 @@
+ - 기본 문법으로 작성한 App_BasicGrammer.vue와 
+   같은 내용을 컴포지션으로 작성한 App_Composion basec.vue와 비교해서 보기
+
+<template>
+    <h1 @click="increase">
+        {{ count }} / {{ doubleCount }}
+    </h1>
+    <h1 @click="changeMessage">
+        {{ message }} / {{ revursedMessage }}
+    </h1>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            count: 0,
+            message: 'Hello World!'
+        }
+    },
+    computed: {
+        doubleCount() {
+            return this.count * 2
+        },
+        revursedMessage() {
+            this.message.split('').reverse().join('')
+        }
+    },
+    watch: {
+        message(newValue) {
+            console.log(newValue)
+        }
+    },
+    
+    //라이프사이클 적용
+    created() {
+        console.log(this.message)
+    },
+    mounted() {
+        console.log(this.count)
+    },
+    methods: {
+        increase() {
+            this.count += 1
+        },
+        changeMessage() {
+            this.message = 'Good?!'
+        }
+    }
+}
+
+</script>
--- src/App_Composition basic.vue
+++ src/App_Composition basic.vue
@@ -3,7 +3,7 @@
   : 기본적으로 반응성X(데이터 변화에 따른 화면 갱신X )
   : 반응성을 주기 위해 ref 객체를 함수처럼 활용 
     -> ref가 반응성을 가지는 객체를 반환함.
-
+    
 <template>
     <div @click="increase">
         {{ count }}
